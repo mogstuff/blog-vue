5163a1bf946ca9fffe3e2904f45b22abeff8cd05
Mark Morgan <markmorgan.mcp@gmail.com>
2021-03-21 16:53:55 +0000

    about component and routing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">  
+<nav>
+  <router-link to="/" class="nav-link">Home</router-link>
+<router-link to="/about" class="nav-link">About</router-link>
+</nav>
+   <router-view/>
   </div>
+
+
+
 </template>
 
+
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+
   },
 };
 </script>
