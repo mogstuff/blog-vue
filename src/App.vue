3a0e1b5e028dddc19b687cced209b5d169be1889
Mark Morgan <markmorgan.mcp@gmail.com>
2021-03-21 16:20:49 +0000

    added stylesheets

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,8 @@ export default {
 };
 </script>
 
+
+
 <style>
 
 @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
@@ -23,12 +25,9 @@ export default {
 @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
 @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
 
-#app {
-  font-family: Nunito, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/css/colours.css';
+@import './assets/css/layout.css';
+@import './assets/css/typography.css';
+
+
 </style>
